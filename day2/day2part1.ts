74f9b217972e581bed282e4313ff262e1bba0c38
langenre <langenre@gmail.com>
2019-12-02 18:46:57 +0100

    Add day2 solution

---
+++ day2/day2part1.ts
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('day2/input.txt', 'utf8')
+    .split(',')
+    .map(Number);
+
+let position = 0;
+input[1] = 12;
+input[2] = 2;
+while(input[position] !== 99) {
+    const operator = input[position];
+    const num1 = input[input[position + 1]];
+    const num2 = input[input[position + 2]];
+    const outputPos = input[position + 3];
+    input[outputPos] = operator === 1 ? num1 + num2 : num1 * num2;
+    position += 4;
+}
+console.log(input[0]);
---
+++ day2/day2part2.ts
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('day2/input.txt', 'utf8')
+    .split(',')
+    .map(Number);
+
+const expected = 19690720;
+for(let noun = 0; noun <= 99; noun++) {
+    for(let verb= 0; verb <= 99; verb++) {
+        let draft = [...input];
+        let position = 0;
+        draft[1] = noun;
+        draft[2] = verb;
+        while(draft[position] !== 99) {
+            const operator = draft[position];
+            const num1 = draft[draft[position + 1]];
+            const num2 = draft[draft[position + 2]];
+            const outputPos = draft[position + 3];
+            draft[outputPos] = operator === 1 ? num1 + num2 : num1 * num2;
+            position += 4;
+        }
+        if (draft[0] === expected) {
+            console.log(100*noun+verb);
+        }
+    }
+}
